a9c1715f91846f7ab0e6a55888dbd90bdff23848
Yotic <yoticcitoy@gmail.com>
2025-03-17 18:48:53 +0300

    Minor changes. The commit before refactoring.

--- Korn.Shared.Internal/GlobalExtensions.cs
+++ Korn.Shared.Internal/GlobalExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 public static class KornSharedGlobalExtensions
 {
@@ -9,5 +10,65 @@ public static class KornSharedGlobalExtensions
 #elif NET472
         return self.Split(new char[] { character }, options);
 #endif
+    }
+
+    public static string[] SplitEx(this string self, string splitter, StringSplitOptions options = StringSplitOptions.None)
+    {
+#if NET8_0
+        return self.Split(splitter, options);
+#elif NET472
+        return self.Split(new string[] { splitter }, options);
+#endif
+    }
+
+    public static MethodInfo GetMethodEx(this Type type, string name)
+    {
+        var methods = type.GetRuntimeMethods();
+        foreach (var method in methods)
+            if (method.Name == name)
+                return method;
+        return null;
+    }
+
+    public static MethodInfo GetMethodEx(this Type type, string name, params Type[] arguments)
+    {
+        var methods = type.GetRuntimeMethods();
+        foreach (var method in methods)
+            if (method.Name == name)
+            {
+                var parameters = method.GetParameters();
+                if (arguments.Length != parameters.Length)
+                    continue;
+
+                for (var argIndex = 0; argIndex < arguments.Length; argIndex++)
+                    if (arguments[argIndex] != parameters[argIndex].ParameterType)
+                        continue;
+
+                return method;
+            }
+        return null;
+    }
+
+    public static Type[] GetArgumentsEx(this MethodInfo method)
+    {
+        var parameters = method.GetParameters();
+        if (method.IsStatic)
+        {
+            var arguments = new Type[parameters.Length];
+            for (var i = 0; i < parameters.Length; i++)
+                arguments[i] = parameters[i].ParameterType;
+
+            return arguments;
+        }
+        else
+        {
+            var arguments = new Type[parameters.Length + 1];
+            for (var i = 0; i < parameters.Length; i++)
+                arguments[i + 1] = parameters[i].ParameterType;
+
+            arguments[0] = method.DeclaringType;
+
+            return arguments;
+        }
     }
 }
--- Korn.Shared.Internal/KornSharedInternal.cs
+++ Korn.Shared.Internal/KornSharedInternal.cs
@@ -4,6 +4,21 @@ namespace Korn.Shared.Internal
 {
     public static class KornSharedInternal
     {
+        static KornSharedInternal()
+        {
+
+        }
+
+        public const string
+            Net8TargetVersion = "net8",
+            Net472TargetVersion = "net472",
+            CurrentTargetVersion =
+#if NET8_0
+            Net8TargetVersion;
+#elif NET472
+            Net472TargetVersion;
+#endif 
+        
         public const string RootDirectory = @"C:\Program Files\Korn";
         public static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings()
         {
